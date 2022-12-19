276391406a00d6e57e39f9628121c0c8eeaadf1c
root <root@vmi1019623.contaboserver.net>
2022-12-19 14:03:00 +0100

    update postgres-nest

--- Docker/dockerfile/ubuntu/postgres-nest/start-apijmvpnest.sh
+++ Docker/dockerfile/ubuntu/postgres-nest/start-apijmvpnest.sh
@@ -5,36 +5,28 @@ set -e
 bash /root/ub-base.sh
 
 config_git(){
+    rm -rf /var/www/html/$REPO_GITHUB
     mkdir -p /var/www/html/$REPO_GITHUB
     cd /var/www/html/$REPO_GITHUB
     git init
-    # git remote add origin $URL_GITHUB
+    git remote add origin $URL_GITHUB
     git pull origin master
 }
 
 config_npm(){
     npm i -g @nestjs/cli
     npm install --force
-    echo "DB_HOST=194.61.28.54
-DB_PORT=5435
-DB_USERNAME=jose
-DB_NAME=IncidenciasNest
-DB_PASSWORD=pswIncidenciasNest
+    echo "DB_HOST=$DB_HOST
+DB_PORT=$DB_PORT
+DB_USERNAME=$DB_USERNAME
+DB_NAME=$DB_NAME
+DB_PASSWORD=$DB_PASSWORD
 
-PORT=3000
-HOST_API=http://localhost:3000/api
+PORT=$PORT
+HOST_API=$HOST_API
 
-JWT_SECRET=MIclaveSecreta" > .env
-
-sed -i 's/5432/5435/' src/app.module.ts
-
-
-# sed -i 's/process.env.DB_HOST/"'194.61.28.54'"/' src/app.module.ts
-# sed -i 's/"'5435'"/5435/' src/app.module.ts
-# sed -i 's/process.env.DB_USERNAME/"'jose'"/' src/app.module.ts
-# sed -i 's/process.env.DB_NAME/"'IncidenciasNest'"/' src/app.module.ts
-# sed -i 's/process.env.DB_PASSWORD/"'pswIncidenciasNest'"/' src/app.module.ts
-
+JWT_SECRET=$JWT_SECRET" > .env
+    sed -i 's/5432/$DB_PORT/' src/app.module.ts
     npm run start:dev
 }
 main(){
