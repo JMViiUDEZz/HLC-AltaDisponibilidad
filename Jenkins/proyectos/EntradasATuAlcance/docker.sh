5f6337d269d0b8d3e3dff19eba602d434a3f078b
root <root@vmi1019623.contaboserver.net>
2023-02-13 18:29:29 +0100

    delete docker.sh

--- Jenkins/proyectos/EntradasATuAlcance/docker.sh
+++
@@ -1,5 +0,0 @@
-# Comandos Docker
-cd docker-compose/ubuntu/
-docker compose -f ub-node-nginx-nest.yml build --no-cache
-docker compose -f ub-node-nginx-nest.yml down
-docker compose -f ub-node-nginx-nest.yml up -d --remove-orphans
