9480fe5ba09f00def371eba03d0b28b41e556666
root <root@vmi1019623.contaboserver.net>
2023-02-20 19:16:11 +0100

    last commit

--- Jenkins/proyectos/EntradasATuAlcance/EntradasATuAlcance.groovy
+++ Jenkins/proyectos/EntradasATuAlcance/EntradasATuAlcance.groovy
@@ -3,7 +3,7 @@
 job('Job-EntradasATuAlcance') {
     description('Job Padre para crear la Tarea Hija que despliegue el Proyecto EntradasATuAlcance')
     scm {
-        git('https://github.com/JMViiUDEZz/Proyecto-EntradasATuAlcance.git','master') {
+        git('https://github.com/JMViiUDEZz/Proyecto-EntradasATuAlcance.git', 'master') {
             node ->
                 node / gitConfigName('JMViiUDEZz')
                 node / gitConfigEmail('jmviudezp2003@gmail.com')
