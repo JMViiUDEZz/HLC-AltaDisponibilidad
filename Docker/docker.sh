c2a76fb2cb208e7625fe3faf8ad966ab7ec6526c
root <root@vmi1019623.contaboserver.net>
2023-02-20 13:27:15 +0100

    last commit

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -1,4 +1,4 @@
 # Comandos Docker
-docker compose -f build/docker-compose/ubuntu/ub-postgres-node-nginx-nest.yml build --no-cache
-docker compose -f build/docker-compose/ubuntu/ub-postgres-node-nginx-nest.yml down
-docker compose -f build/docker-compose/ubuntu/ub-postgres-node-nginx-nest.yml up -d --remove-orphans
+docker compose -f build/docker-compose/ubuntu/ub-node-nginx.yml build --no-cache
+docker compose -f build/docker-compose/ubuntu/ub-node-nginx.yml down
+docker compose -f build/docker-compose/ubuntu/ub-node-nginx.yml up -d --remove-orphans
