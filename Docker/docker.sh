7348cdb07a913fd9e8107219c2b7b1a6fd4df8d9
root <root@vmi1019623.contaboserver.net>
2023-02-23 09:33:38 +0100

    another commit

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -1,5 +1,6 @@
 # Comandos Docker
-docker compose -f build/Docker/docker-compose/ubuntu/ub-node-nginx.yml build --no-cache
+# docker compose -f build/Docker/docker-compose/ubuntu/ub-node-nginx.yml build --no-cache
+docker compose -f build/Docker/docker-compose/ubuntu/ub-node-nginx.yml build
 docker compose -f build/Docker/docker-compose/ubuntu/ub-node-nginx.yml down
 # docker compose -f build/Docker/docker-compose/ubuntu/ub-postgres-nest.yml up -d --remove-orphans
-docker compose -f build/Docker/docker-compose/ubuntu/ub-node-nginx.yml up -d --remove-orphans
+docker compose -f build/Docker/docker-compose/ubuntu/ub-node-nginx.yml up
--- Docker/dockerfile/ubuntu/postgres-nest/ub-postgres-nest-dev.sh
+++ Docker/dockerfile/ubuntu/postgres-nest/ub-postgres-nest-dev.sh
@@ -5,15 +5,17 @@ set -e
 bash /root/ub-base.sh
 
 config_git(){
-    rm -rf /var/www/html/$REPO_GITHUB
-    mkdir -p /var/www/html/$REPO_GITHUB
-    cd /var/www/html/$REPO_GITHUB
-    git init
-    git remote add origin $URL_GITHUB
-    git pull origin master
+    # rm -rf /var/www/html/$REPO_GITHUB
+    # mkdir -p /var/www/html/$REPO_GITHUB
+    # cd /var/www/html/$REPO_GITHUB
+    rm -rf /var/www/html/api
+    mkdir -p /var/www/html
+    cd /var/www/html
+    git clone https://github.com/JMViiUDEZz/EntradasATuAlcance-Backend.git api
 }
 
 config_npm(){
+    cd /var/www/html/api
     npm i -g @nestjs/cli
     npm install --force
     echo "DB_HOST=$DB_HOST
