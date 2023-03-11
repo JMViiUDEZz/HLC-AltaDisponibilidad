6493fc9f1bfa2856e6c64ba9a64acd1242223d1c
root <root@vmi1019623.contaboserver.net>
2023-03-11 11:21:08 +0100

    last commit

--- Jenkins/proyectos/EntradasATuAlcance/EntradasATuAlcanceBackend.groovy
+++ Jenkins/proyectos/EntradasATuAlcance/EntradasATuAlcanceBackend.groovy
@@ -14,6 +14,6 @@ job('Job-EntradasATuAlcance-Backend') {
         githubPush()
     }
     steps {
-        shell('bash docker-node.sh && docker-nginx.sh')
+        shell('sh docker-node.sh && docker-nginx.sh')
     }
 }
