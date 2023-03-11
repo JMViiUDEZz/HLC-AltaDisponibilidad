fccb9d163537fc4e0315ddbd9a75897982060905
root <root@vmi1019623.contaboserver.net>
2023-03-11 10:57:37 +0100

    another commit

--- Jenkins/proyectos/EntradasATuAlcance/EntradasATuAlcanceBackend.groovy
+++ Jenkins/proyectos/EntradasATuAlcance/EntradasATuAlcanceBackend.groovy
@@ -14,6 +14,6 @@ job('Job-EntradasATuAlcance-Backend') {
         githubPush()
     }
     steps {
-        shell('bash docker.sh')
+        shell('bash docker-node.sh &&  docker-nginx.sh')
     }
 }
